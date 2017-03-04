0facdae3ec487e61a894970b8c2ef93c3fa28152
zhengminmin <21413244@qq.com>
2017-03-04 18:52:13 +0800

    first commit

---
+++ gitT/gitT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitT
+//
+//  Created by iRunnings on 2017/3/4.
+//  Copyright © 2017年 iRunnings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitT/gitT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitT
+//
+//  Created by iRunnings on 2017/3/4.
+//  Copyright © 2017年 iRunnings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
